0f6327680d6cd47e335c790df0948b9c7de601a2
Madhavgarg98 <madhavgarg3300@gmail.com>
2025-03-25 11:02:26 +0530

    vit

---
+++ src/components/Card.jsx
@@ -0,0 +1,43 @@
+
+
+import React from 'react'
+import "./Card.css"
+export default function Card() {
+  return (
+    <>
+    <div className='container'>
+    <div className='profile'>
+
+      <h1>Madhav Garg</h1>
+      <p>👨‍🎓 I am a CSE Student</p>
+      <p>👨‍💻 I want to work with top companines </p>
+      <p>🙌 Respected student of Chitkara X Kalvium </p>
+    </div>
+
+    <div className='profile'>
+      <h1>Tanmay Bhatt </h1>
+      <p>👨‍🎓 I am a CSE Student</p>
+      <p>👨‍💻 I want to work with top Dye-makers </p>
+      <p>🙌 Respected student of Chitkara X Kalvium </p>
+    </div>
+
+    <div className='profile'>
+      <h1>Nishant</h1>
+      <p>👨‍🎓 I am a CSE Student</p>
+      <p>👨‍💻 I want to work with top Billioners</p>
+      <p>🙌 Respected student of Chitkara X Kalvium </p>
+    </div>
+    
+    <div className='profile'>
+      <h1>Moksh Sharma</h1>
+      <p>👨‍🎓 I am a CSE Student</p>
+      <p>👨‍💻 I want to work with top Coding platform</p>
+      <p>🙌 Respected student of Chitkara X Kalvium </p>
+    </div>
+    </div>
+
+        
+    </>
+ 
+  )
+}
